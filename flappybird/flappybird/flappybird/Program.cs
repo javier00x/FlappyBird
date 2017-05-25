a754e4bf1d35627054c7cc7faaeda7d5c85488c5
javierf7900@gmail.com <Sababa00>
2017-05-25 14:11:42 -0400

    idk

---
+++ flappybird/flappybird/flappybird/Program.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace flappybird
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (FlappyGame game = new FlappyGame())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
